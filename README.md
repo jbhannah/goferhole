cf1911c3aef7a17f25f46691dca95e8a35339004
Jesse B. Hannah <jesse@jbhannah.net>
2019-02-13 21:30:09 -0700

    Initial commit; basic forwarding

---
+++ README.md
@@ -0,0 +1,18 @@
+# Goferhole
+
+A home DNS server for:
+
+- [ ] Blocking ads
+- [ ] Internal resolution
+- [ ] Secure forwarding
+
+Built with [miekg/dns][].
+
+## Copyright
+
+Copyright © 2019 [Jesse B. Hannah][]. Licensed under the MIT License (see
+[LICENSE][]).
+
+[miekg/dns]: https://github.com/miekg/dns
+[jesse b. hannah]: https://jbhannah.net
+[license]: LICENSE
---
+++ cmd/goferhole/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+
+	"github.com/miekg/dns"
+)
+
+func main() {
+	port := 8053
+
+	c := &dns.Client{Net: "tcp-tls"}
+
+	dns.HandleFunc(".", func(w dns.ResponseWriter, r *dns.Msg) {
+		rx, _, err := c.Exchange(r, "1.1.1.1:853")
+		if err != nil {
+			log.Printf("Lookup failed: %s\n", err.Error())
+		}
+		w.WriteMsg(rx)
+	})
+
+	go func() {
+		srv := &dns.Server{Addr: ":" + strconv.Itoa(port), Net: "udp"}
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatalf("Failed to set udp listener: %s\n", err.Error())
+		}
+	}()
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	s := <-sig
+	log.Fatalf("Signal (%v) received, stopping\n", s)
+}
